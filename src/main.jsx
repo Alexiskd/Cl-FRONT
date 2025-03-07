d83a93abae3316db60271d5e2b9ff30f6d6b042f
alexis bouvet <alexisdashlane@gmail.com>
2025-03-07 12:16:34 +0100

    Mise à jour du lockfile et suppression de package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from '@emotion/react';
 import App from './SiteWeb/App';
 import { theme } from './theme';
-import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-  <ThemeProvider theme={theme}>
+  <BrowserRouter>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
-</BrowserRouter>
+  </BrowserRouter>
 );
