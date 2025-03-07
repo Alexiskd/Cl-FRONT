a8b8667f0d8c6214bfaccfd9965c0fc5166eb057
Alexiskd <160446128+Alexiskd@users.noreply.github.com>
2025-03-07 12:36:42 +0100

    Create sitemap-generator.js

---
+++ sitemap-generator.js
@@ -0,0 +1,34 @@
+// sitemap-generator.js
+const path = require('path');
+const Sitemap = require('react-router-sitemap').default;
+
+// Liste des routes statiques à inclure dans le sitemap
+const routes = [
+  '/',
+  '/index.php',
+  '/trouvez.php',
+  '/catalogue-cles-coffre.php',
+  '/catalogue-telecommandes.php',
+  '/badges.php',
+  '/services.php',
+  '/cle/double-de-cle.html',
+  '/zone.php',
+  '/paiement_devis.php',
+  '/recherche.php',
+  '/contact.php',
+  '/commande-success',
+  '/commande-cancel',
+  '/devis.php',
+  '/qui.php',
+  '/commande-panier',
+  '/upload-multiple',
+  '/politique-confidentialite',
+  '/mentions-legales',
+  '/conditions-generales'
+];
+
+new Sitemap(routes)
+  .build('https://www.votresite.com') // Remplacez par l'URL de votre site
+  .save(path.resolve('./public/sitemap.xml'));
+
+console.log('Sitemap généré avec succès.');
